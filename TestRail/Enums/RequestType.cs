9479481ca189ccec298a23d7b7b31afcb0afb986
Chris Munoz <cmunoz@extraspace.com>
2019-04-16 09:17:10 -0600

    Created a new enum to clearly label request.

---
+++ TestRail/Enums/RequestType.cs
@@ -0,0 +1,16 @@
+﻿using EnumStringValues;
+
+namespace TestRail.Enums
+{
+    /// <summary>These enums are used to label what kind of request you are building.</summary>
+    public enum RequestType
+    {
+        /// <summary>Used for GET requests.</summary>
+        [StringValue("GET")]
+        Get,
+
+        /// <summary>Used for POST requests.</summary>
+        [StringValue("POST")]
+        Post,
+    }
+}
