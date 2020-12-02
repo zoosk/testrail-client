dbbf81899360aa1462e35319dde7258a7fff6ea9
Chris Munoz <cmunoz@extraspace.com>
2020-12-01 20:58:58 -0700

    Finished setting up the test

--- TestRail.Tests/ClientTests.cs
+++ TestRail.Tests/ClientTests.cs
@@ -1,12 +1,28 @@
+using Microsoft.Extensions.Configuration;
+using System.IO;
 using Xunit;
 
 namespace TestRail.Tests
 {
     public class ClientTests
     {
-        [Fact]
-        public void Test1()
+        private readonly IConfigurationRoot _configuration;
+
+        public ClientTests()
         {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        [Fact]
+        public void Authentication_Is_Successful()
+        {
+            var section = _configuration.GetSection("TestRail");
+            var client = new TestRailClient(section["baseurl"], section["username"], section["password"]);
+
+            Assert.NotNull(client.Projects);
         }
     }
 }
